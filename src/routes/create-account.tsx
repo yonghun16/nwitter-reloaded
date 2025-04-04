b0291c4769d02f916ddd747f7846a9e414bcac77
yonghun16 <yonghun16@naver.com>
2025-04-05 05:08:11 +0900

    create account

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react"
 import { styled } from "styled-components";
+import { auth } from "../firebase";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 const Wrapper = styled.div`
   height: 100%;
@@ -43,6 +46,7 @@ const Error = styled.span`
  `;
 
 export default function CreateAccount() {
+  const navigate = useNavigate();
   const [isLoading, setLoading] = useState(false);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -61,12 +65,23 @@ export default function CreateAccount() {
     }
   }
 
-  const onSubmit = (evnet: React.FormEvent<HTMLFormElement>) => {
+  const onSubmit = async (evnet: React.FormEvent<HTMLFormElement>) => {
     evnet.preventDefault();
+    if(isLoading || name === "" || email === "" || password === "") {
+      return;
+    }
     try {
-      // create an account
-      // set the name of the user.
-      // redirect to the home page.
+      setLoading(true);
+      const credentials = await createUserWithEmailAndPassword(
+        auth, 
+        email, 
+        password
+      );
+      console.log(credentials.user);
+      await updateProfile(credentials.user, {    // 사용자 계정 이름 받아오기
+        displayName: name
+      });
+      navigate("/");
     } catch (error) {
 
     } finally {
@@ -77,7 +92,7 @@ export default function CreateAccount() {
 
   return (
     <Wrapper>
-      <Title>Log into 𝕏</Title>
+      <Title>Join 𝕏</Title>
       <Form onSubmit={onSubmit}>
         <Input
           onChange={onChange}
