23212953728269c83f4590c93d3211c643f9ce6b
yonghun16 <yonghun16@naver.com>
2025-04-05 01:37:07 +0900

    firebase setup

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD9i9eUrtolWAgmDsh0OjzyKRbm8m1O1eM",
+  authDomain: "nwitter-reloaded-95320.firebaseapp.com",
+  projectId: "nwitter-reloaded-95320",
+  storageBucket: "nwitter-reloaded-95320.firebasestorage.app",
+  messagingSenderId: "356178270482",
+  appId: "1:356178270482:web:6aafaef9f1de9d50b3bbcb"
+};
+
+const app = initializeApp(firebaseConfig);
