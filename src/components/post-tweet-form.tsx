389a9c38965a3e488a47467fc8b41341ab10daf7
yonghun16 <yonghun16@naver.com>
2025-04-06 02:12:20 +0900

    Post Tweet Form

---
+++ src/components/post-tweet-form.tsx
@@ -0,0 +1,94 @@
+import styled from "styled-components"
+import { useState } from "react"
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const TextArea = styled.textarea`
+  border: 2px solid white;
+  padding: 20px;
+  border-radius: 20px;
+  font-size: 16px;
+  color: white;
+  background-color: black;
+  width: 100%;
+  resize: none;  /* resize 기능 제거 */
+  box-sizing: border-box;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  &::placeholder {
+    font-size: 16px;
+  }
+  &:focus {
+    outline: none;
+    border-color: #1d9bf0;
+  }
+`;
+
+const AttachFileButton = styled.label`
+  padding: 10px 0px;
+  color: #1d9bf0;
+  text-align: center;
+  border-radius: 20px;
+  border: 1px solid #1d9bf0;
+  font-size: 14px;
+  font-weight: 600;
+  cursor: pointer;
+`;
+
+const AttachFileInput = styled.input`
+  display: none;
+`;
+
+const SubmitBtn = styled.input`
+  background-color: #1d9bf0;
+  color: white;
+  padding: 10px 0px;
+  border-radius: 20px;
+  border: none;
+  font-size: 16px;
+  font-weight: 600;
+  cursor: pointer;
+  &:hover,
+  &:active {
+    opacity: 0.9;
+  }
+`;
+
+export default function PostTweetForm() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [tweet, setTweet] = useState("");
+  const [file, setFile] = useState<File | null>(null);
+  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTweet(e.target.value);
+  }
+
+  const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target;
+    if (files && files.length === 1) {
+      setFile(files[0]);
+    }
+  }
+
+  return (
+    <Form>
+      <TextArea
+        rows={5}
+        maxLength={180}
+        onChange={onChange}
+        value={tweet}
+        placeholder="What's happening?"
+      />
+      <AttachFileButton htmlFor="file">{file ? "Photo added ✅" : "Add photo"} </ AttachFileButton>
+      <AttachFileInput 
+        type="file"
+        onChange={onFileChange}
+        id="file" 
+        accept="image/*" 
+      />
+      <SubmitBtn type="submit" value={isLoading ? "Posting..." : "Post Tweet"} />
+    </Form>
+  );
+}
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,12 +1,12 @@
-import { auth } from "../firebase"
+import PostTweetForm from "../components/post-tweet-form";
+import styled from "styled-components";
+
+const Wrapper = styled.div``;
 
 export default function Home() {
-  const logOut = () => {
-    auth.signOut();
-  }
   return (
-    <>
-      <h1><button onClick={logOut}>Logout</button></h1>
-    </>
-  )
+    <Wrapper>
+      <PostTweetForm />
+    </Wrapper>
+  );
 }
