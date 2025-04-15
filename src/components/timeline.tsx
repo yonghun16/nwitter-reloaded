6aea3b30ec6c453cc1afed6e229436725501cf10
yonghun16 <yonghun16@naver.com>
2025-04-15 20:41:01 +0900

    Realtime Tweets

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import styled from "styled-components";
 import { db } from "../firebase";
-import { getDocs, collection, query, orderBy } from "firebase/firestore";
+import { getDocs, collection, query, orderBy, onSnapshot, limit, Unsubscribe } from "firebase/firestore";
 import Tweet from "./tweet";
 
 // interfaces
@@ -15,31 +15,44 @@ export interface ITweet {
 }
 
 const Wrapper = styled.div`
-
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
 `;
 
 export default function Timeline() {
   const [tweets, setTweets] = useState<ITweet[]>([]);
 
-  const fetchTweets = async () => {
-    const tweetsQuery = query(        // Firestore Query을 통하여 data 가져오기
-      collection(db, "tweets"),
-      orderBy("createdAt", "desc")
-    )
-
-    const spanshot = await getDocs(tweetsQuery);
-
-    const tweets = spanshot.docs.map((doc) => {
-      const { tweet, createdAt, username, userId, image } = doc.data();
-      return { tweet, createdAt, username, userId, image, id: doc.id };
-    });
-
-    // tweets state update
-    setTweets(tweets);
-  };
-
   useEffect(() => {
+    let unsubscribe : Unsubscribe | null = null;
+    const fetchTweets = async () => {
+      const tweetsQuery = query(        // Firestore Query을 통하여 data 가져오기
+        collection(db, "tweets"),
+        orderBy("createdAt", "desc"),
+        limit(25)
+      )
+
+      /*
+      const spanshot = await getDocs(tweetsQuery);
+  
+      const tweets = spanshot.docs.map((doc) => {
+        const { tweet, createdAt, username, userId, image } = doc.data();
+        return { tweet, createdAt, username, userId, image, id: doc.id };
+      });
+      */
+
+      unsubscribe = onSnapshot(tweetsQuery, (snapshot) => {
+        const tweets = snapshot.docs.map((doc) => {
+          const { tweet, createdAt, username, userId, image } = doc.data();
+          return { tweet, createdAt, username, userId, image, id: doc.id };
+        });
+        setTweets(tweets);
+      });
+    };
     fetchTweets();
+    return () => {
+      unsubscribe && unsubscribe();
+    }
   }, []);
 
   return (
--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -1,30 +1,34 @@
+import { styled } from "styled-components";
 import { ITweet } from "./timeline";
-import styled from "styled-components";
 
 const Wrapper = styled.div`
-  display: gird;
+  display: grid;
   grid-template-columns: 3fr 1fr;
   padding: 20px;
   border: 1px solid rgba(255, 255, 255, 0.5);
   border-radius: 15px;
 `;
 
-const Column = styled.div``;
-
-const Username = styled.span`
-  font-weight: 600;
-  font-size: 14px;
-`;
-
-const Payload = styled.p`
-  margin: 10px 0px;
-  font-size: 18px;
+const Column = styled.div`
+  &:last-child {
+    place-self: end;
+  }
 `;
 
 const Photo = styled.img`
   width: 100px;
   height: 100px;
   border-radius: 15px;
+`;
+
+const Username = styled.span`
+  font-weight: 600;
+  font-size: 15px;
+`;
+
+const Payload = styled.p`
+  margin: 10px 0px;
+  font-size: 18px;
 `;
 
 export default function Tweet({ username, image, tweet }: ITweet) {
@@ -34,11 +38,11 @@ export default function Tweet({ username, image, tweet }: ITweet) {
         <Username>{username}</Username>
         <Payload>{tweet}</Payload>
       </Column>
+      <Column>
       {image ? (
-        <Column>
           <Photo src={`${image}`} />
-        </Column>
       ) : null}
+      </Column>
     </Wrapper>
   )
 }
