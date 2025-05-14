a4d49c60c6c6fc05e9725964e16f2ce58c275057
yonghun16 <yonghun16@naver.com>
2025-05-14 19:46:41 +0900

    github login reload

--- src/components/github-btn.tsx
+++ src/components/github-btn.tsx
@@ -1,9 +1,11 @@
-import styled from "styled-components";
-import { GithubAuthProvider } from "firebase/auth";
-import { auth } from "../firebase";
-import { signInWithPopup } from "firebase/auth";
+/* library import */
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { auth } from "../firebase";
+import { signInWithPopup, GithubAuthProvider  } from "firebase/auth";
 
+
+/* styled components */
 const Button = styled.span`
   margin-top: 50px;
   background-color: white;
@@ -26,18 +28,21 @@ const Logo = styled.img`
   height: 30px;
 `;
 
+
+/* GithubButton Component */
 export default function GithubButton() {
   const navigate = useNavigate();
   const onClick = async () => {
     try {
-      const provider = new GithubAuthProvider();  // 인증제공자
-      await signInWithPopup(auth, provider);      // signInWithPopup 함수는 팝업 창을 띄워서 사용자가 GitHub 계정으로 로그인 가능
-      navigate("/");
-    } catch (error) {
-      console.log(error);
+      const provider = new GithubAuthProvider();    // 인증제공자
+      await signInWithPopup(auth, provider);        // signInWithPopup 함수는 팝업 창을 띄워서 사용자가 GitHub 계정으로 로그인 가능
+      navigate("/");                                // 로그인 후 home 페이지로 이동
+    } catch (error) {                               // 에러 발생시
+      console.log(error);                           // 에러 출력
     }
   }
 
+  // render - Dom으로 리턴
   return (
     <Button onClick={onClick}>
       <Logo src="/github-logo.svg" /> Connect with Github
